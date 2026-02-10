e7f38f7ef398d4095761e8069ed7f1c1053d2961
codieSam <samratbelbase408@gmail.com>
2026-02-10 16:19:49 +0000

    Commiting into develop

--- lab_week_part3.py
+++ lab_week_part3.py
@@ -37,7 +37,7 @@ elif(input_value == 1):
     converted_temp = (forenhite_input - 32) * 5/9
 
 
-    print("The tempoerature in Forenhite is", (converted_temp), "Celcius")
+    print("The tempoerature in Forenhite is", (converted_temp), "Celcius Degree")
 else:
     print("Invalid input ! Please type 1 or 0 only.")
 
@@ -46,4 +46,4 @@ else:
 
 celsius_input = float(input())
 converted_temp = (celsius_input * 9/5) + 32
-print("The tempoerature in Forenhite is", (converted_temp), "Forenhite")
+print("The tempoerature in Forenhite is", (converted_temp), "Forenhite Degree")
